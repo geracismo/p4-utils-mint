4a7e05a5276ef9c2fe61cb3b0dd7585d12670f67
Simone Geraci <61033848+geracismo@users.noreply.github.com>
2025-02-13 11:35:57 -0600

    Update install-p4-dev.sh

--- install-tools/install-p4-dev.sh
+++ install-tools/install-p4-dev.sh
@@ -56,7 +56,7 @@ FRROUTING_COMMIT="frr-8.5" # Mar 25 2021
 function do_os_message() {
     1>&2 echo "Found ID ${ID} and VERSION_ID ${VERSION_ID} in /etc/os-release"
     1>&2 echo "This script only supports these:"
-    1>&2 echo "    ID ubuntu, VERSION_ID in 20.04 22.04"
+    1>&2 echo "    ID linuxmint, VERSION_ID in 20.04 22.04"
     1>&2 echo ""
     1>&2 echo "Proceed installing at your own risk."
 }
